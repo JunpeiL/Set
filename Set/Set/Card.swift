2b4a960262e1e3b9e12eacebdb514a13eeb03d5b
JunpeiL <junpei.liang@gmail.com>
2018-07-22 22:34:12 -0700

    Assignment2

---
+++ Set/Set/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  Set
+//
+//  Created by offline on 2018/6/19.
+//  Copyright © 2018 JoP. All rights reserved.
+//
+
+import Foundation
---
+++ Set/Set/Set.swift
@@ -0,0 +1,9 @@
+//
+//  Set.swift
+//  Set
+//
+//  Created by offline on 2018/6/19.
+//  Copyright © 2018 JoP. All rights reserved.
+//
+
+import Foundation
